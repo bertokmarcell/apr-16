72656f308f4fc15e7494b98a1d4c82a89ac86ed0
bertokmarcell <bertokmarcell@gmail.com>
2026-04-16 10:04:15 +0200

    kiindulas

---
+++ adat.js
@@ -0,0 +1,50 @@
+export const ADATOK = [{
+    nev: "Curry",
+    leir: "Fűszeres, aromás étel vagy fűszerkeverék, főleg indiai és dél-ázsiai konyhákból, gyakran szószos formában hússal vagy zöldségekkel.",
+    kep: "dfsg",
+    ar: 36267676,
+    src: "kepekm/1.png",
+    db: 0
+},
+{
+    nev: "Thragg",
+    leir: "Thragg: A Thragg az Invincible képregény- és sorozatuniverzum egyik fő antagonistája. A Viltrumita Birodalom nagyvezére, rendkívül erős, kegyetlen és stratégiai zseni, aki a viltrumiták uralmának fenntartására törekszik.",
+    kep: "dfsg",
+    ar: 25,
+    src: "kepekm/2.png",
+    db: 0
+},
+{
+    nev: "Tej",
+    leir: "Alapvető, tápanyagban gazdag ital, amelyet leggyakrabban tehénből nyernek, és sok étel alapja.",
+    kep: "dfsg",
+    ar: 5324,
+    src: "kepekm/3.png",
+    db: 0
+},
+{
+    nev: "Pizza",
+    leir: "Olasz eredetű étel, kelt tésztából, paradicsomszósszal, sajttal és különféle feltétekkel sütve",
+    kep: "dfsg",
+    ar: 567,
+    src: "kepekm/4.png",
+    db: 0,
+},
+{
+    nev: "Steak",
+    leir: "Vastag szelet hús (általában marha), serpenyőben vagy grillen sütve, gyakran szaftosan tálalva.",
+    kep: "dfsg",
+    ar: 3245,
+    src: "kepekm/5.png",
+    db: 0,
+},
+{
+    nev: "Tengeri só",
+    leir: "(tengeri só): Tengervíz párologtatásával nyert só, enyhébb ízű és gyakran durvább szemcséjű, mint a hagyományos konyhasó.",
+    kep: "dfsg",
+    ar: 2415,
+    src: "kepekm/6.png",
+    db: 0,
+},]
+
+
